a88e26831b55490297ebc1d492c0ee6a67c8597e
Dpakkk <pokharelbikash981@gmail.com>
2022-12-15 10:53:25 -0500

    Questions added for further analysis

--- notYetStarted.py
+++ notYetStarted.py
@@ -20,13 +20,22 @@ class Mercedez:
         return self.salesQuantity/self.salesTarget
 
 
-m1 = Mercedez("A-CLASS",2020,"USA",['Black', 'Blue','Navy','White'], 450000, 8000, 3000)
-m2 = Mercedez("B-CLASS*",2020,"USA",['Black', 'Blue','Navy','White'], 550000, 6000, 2000)
-m3 = Mercedez("CLA",2020,"USA",['Black', 'Blue','Navy','White'], 350000, 4000, 3000)
-m4 = Mercedez("C-CLASS",2020,"GERMANY",['Black', 'Blue','Navy','White'], 450000, 14000, 9000)
-m5 = Mercedez("E-CLASS/CLS",2020,"USA",['Black', 'Blue','Navy','White'], 250000, 17000, 16999)
-m6 = Mercedez("S-CLASS/CLS",2020,"USA",['Black', 'Blue','Navy','White'], 450000, 19990, 15349)
-m7 = Mercedez("GLA",2020,"USA",['BLUE', 'YELLOW','MATT BLACK','White'], 430000, 600, 100)
-m8 = Mercedez("GLB",2020,"USA",['SHINY BLACK', 'Blue','Navy','White'], 450000, 500, 250)
-m7 = Mercedez("GLC",2020,"USA",['BLUE', 'YELLOW','MATT BLACK','White'], 420000, 800, 800)
-m8 = Mercedez("GLE",2021,"GERMANY",['G-WAGON MIX COLOR', 'Blue','Navy DARK','White'], 60000, 200, 180)
+A_class = Mercedez("A-CLASS",2020,"USA",['Black', 'Blue','Navy','White'], 450000, 8000, 3000)
+B_Class = Mercedez("B-CLASS*",2020,"USA",['Black', 'Blue','Navy','White'], 550000, 6000, 2000)
+CLA = Mercedez("CLA",2020,"USA",['Black', 'Blue','Navy','White'], 350000, 4000, 3000)
+C_CLASS = Mercedez("C-CLASS",2020,"GERMANY",['Black', 'Blue','Navy','White'], 450000, 14000, 9000)
+E_CLASS = Mercedez("E-CLASS/CLS",2020,"USA",['Black', 'Blue','Navy','White'], 250000, 17000, 16999)
+S_CLASS = Mercedez("S-CLASS/CLS",2020,"USA",['Black', 'Blue','Navy','White'], 450000, 19990, 15349)
+GLA = Mercedez("GLA",2020,"USA",['BLUE', 'YELLOW','MATT BLACK','White'], 430000, 600, 100)
+GLB = Mercedez("GLB",2020,"USA",['SHINY BLACK', 'Blue','Navy','White'], 450000, 500, 250)
+GLC = Mercedez("GLC",2020,"USA",['BLUE', 'YELLOW','MATT BLACK','White'], 420000, 800, 800)
+GLE = Mercedez("GLE",2021,"GERMANY",['G-WAGON MIX COLOR', 'Blue','Navy DARK','White'], 60000, 200, 180)
+
+
+# things to do here
+# Find defeact rate of each models
+# Which models performs better
+# Which model done least sales
+# Models sales meet or not?
+# Interesting insights coming soon in this file
+# code time: 10:50 AM, Thursday, December 15, 2022
